bc5075a3b5bd3f0ca8ed4be528d4a2444d985751
Jonathan Ribarro <Jcribarro@gmail.com>
2017-02-24 22:14:08 -0500

    COMMIT FOR REAL

---
+++ Assets/Scripts/TOGGLEVR.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+
+public class VrModeOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.V))
+        {
+            VRSettings.enabled = !VRSettings.enabled;
+            Debug.Log("Changed VRsettings.enabled to: " + VRSettings.enabled);
+
+        }
+    }
+}
